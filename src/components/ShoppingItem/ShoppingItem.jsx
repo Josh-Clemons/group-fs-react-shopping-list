7c743a9a982e47285a14ecb20be0fc7515d35eca
Abdirahman Farah <abdirahmanfarah122@gmail.com>
2022-11-23 11:31:14 -0600

    shoppingItem code

---
+++ src/components/ShoppingItem/ShoppingItem.jsx
@@ -0,0 +1,14 @@
+
+export default function ShoppingItem({listItem}) {
+
+    return (<>
+        <div className="newItem">
+            <h3>{listItem.name}  </h3>
+            <p>{listItem.qty}   {listItem.unit}</p>
+            {listItem.isPurchased
+                ? <p>PURCHASED</p>
+                : <><button onClick={() => deleteItem(newItem)}>Delete</button>
+                    <button onClick={() => itemPurchased(newItem)}>Purchase</button> </>}
+        </div>
+    </>);
+}
