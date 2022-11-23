e301b473b56f6a9af1adf040ece6bdbe0fc4f4ac
Billy Salinas <wsalinas315@gmail.com>
2022-11-23 10:11:13 -0600

    app.jsx GET written

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
-
-import Header from '../Header/Header.jsx'
+import {useState, useEffect} from 'react';
+import Header from '../Header/Header.jsx';
 import './App.css';
 
 
 function App() {
+
+    let[shoppingList, setShoppingList] = useState([]);
+
+    // Load shoppingList
+    useEffect(() => {
+        getList()
+    }, [])
+
+    // GET call for list from Database
+    const getList = () => {
+        axios.get('/items').then(response => {
+            setShoppingList(response.data);
+        }).catch(error => {
+            alert('Error with App.jsx GET:' + error);
+        })
+    }
+
     return (
         <div className="App">
             <Header />
