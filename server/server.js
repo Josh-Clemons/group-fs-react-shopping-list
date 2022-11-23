c81c477047a75a651107d30f18dd570cf43f11fe
Josh-Clemons <110069447+Josh-Clemons@users.noreply.github.com>
2022-11-23 10:42:57 -0600

    fixed router bug

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
-const itemRouter = require('./routes/items')
+const itemRouter = require('./routes/items');
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for axios requests
