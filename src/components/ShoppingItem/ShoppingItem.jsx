f2a231e15a49c999a0281dec62e52208180f41d5
Abdirahman Farah <abdirahmanfarah122@gmail.com>
2022-11-23 14:27:43 -0600

    finsihed my put id and delete id

--- src/components/ShoppingItem/ShoppingItem.jsx
+++ src/components/ShoppingItem/ShoppingItem.jsx
@@ -1,14 +1,33 @@
+import axios from "axios";
 
 export default function ShoppingItem({listItem}) {
-
+    const deleteItem = () => {
+        console.log(`Delete Item ID`, listItem.id);
+        axios.delete('/items/' + listItem.id)
+        .then(response => {
+        }).catch(error => {
+            console.log('Error in DELETE', error);
+        });
+      }
+    
+    const itemPurchased = () => {
+        console.log('Purchased',listItem.name )
+        axios.put('/items/' + listItem.name)
+        .then((result) => {
+            console.log(`success /items PUT`, id);
+        }).catch((error) => {
+            console.log(`ERROR in /items PUT`, error);
+            
+    })
+    }
     return (<>
         <div className="newItem">
             <h3>{listItem.name}  </h3>
             <p>{listItem.qty}   {listItem.unit}</p>
             {listItem.isPurchased
                 ? <p>PURCHASED</p>
-                : <><button onClick={() => deleteItem(newItem)}>Delete</button>
-                    <button onClick={() => itemPurchased(newItem)}>Purchase</button> </>}
+                : <><button onClick={() => deleteItem()}>Delete</button>
+                    <button onClick={() => itemPurchased()}>Purchase</button> </>}
         </div>
     </>);
 }
